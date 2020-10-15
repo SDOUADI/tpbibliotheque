7de4168343789171e6dd110c5ec4f3572bed511c
Misokasa <misokasa@users.noreply.github.com>
2020-10-15 15:20:55 +0200

    Moi aussi je l'ai modifié !

--- homepagebookshop.php
+++ homepagebookshop.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html>
-    
+
     <head>
         <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      <title>Template Intense</title>
+      <title>Template Sonia DOUADI</title>
 
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
       <link rel="stylesheet" href="css/templateintenselastv.css" />
-       
+
     </head>
-    
+
     <body>
-       
+
           <?php
             require_once 'template/navigation.php';
         ?>
-              
+
           <div class="container-fluid">
               <?php
             require_once 'includes/functions.php';
@@ -24,7 +24,7 @@
 			if($page=="" || $page=="content")
 			{
 			//echo $page;
-			
+
 			   include 'template/content.php';
 			}
 			else if($page=="livre"){
@@ -32,23 +32,23 @@
 			}
               ?>
         </div>
-        
+
         <?php
         include "template/footer.php";
         ?>
-        
-        
-  
-        
-    <script src="../tpweb/js/scripttemplateintense.js" >         
+
+
+
+
+    <script src="../tpweb/js/scripttemplateintense.js" >
     </script>
-        
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
    <script>
        $(document).ready(function(){
-           
+
            var home=$("#home");
            var features=$("#features");
            var pages=$("#pages");
@@ -70,90 +70,90 @@
            var mborder5=$(".mborder5");
            var mborder6=$(".mborder6");
            var mborder7=$(".mborder7");
-           
-            home.mouseover (function(){ 
+
+            home.mouseover (function(){
             sousmenuhome.attr('style','display:block');
             mborder.attr('style','display:block');
-               
+
            });
-           
-            home.mouseout (function(){  
+
+            home.mouseout (function(){
             sousmenuhome.attr('style','display:none');
-               
+
            });
-           
-            features.mouseover (function(){ 
+
+            features.mouseover (function(){
             sousmenufeatures.attr('style','display:block');
             mborder2.attr('style','display:block');
-               
+
            });
-           
-            features.mouseout (function(){  
+
+            features.mouseout (function(){
             sousmenufeatures.attr('style','display:none');
-               
+
            });
-           
-            pages.mouseover (function(){ 
+
+            pages.mouseover (function(){
             sousmenupages.attr('style','display:block');
             mborder3.attr('style','display:block');
-               
+
            });
-           
-            pages.mouseout (function(){  
+
+            pages.mouseout (function(){
             sousmenupages.attr('style','display:none');
-               
+
            });
-           
-            portfolio.mouseover (function(){ 
+
+            portfolio.mouseover (function(){
             sousmenuporfolio.attr('style','display:block');
             mborder4.attr('style','display:block');
-               
+
            });
-           
-            portfolio.mouseout (function(){  
+
+            portfolio.mouseout (function(){
             sousmenuporfolio.attr('style','display:none');
-               
+
            });
-           
-            blog.mouseover (function(){ 
+
+            blog.mouseover (function(){
             sousmenublog.attr('style','display:block');
             mborder5.attr('style','display:block');
-               
+
            });
-           
-            blog.mouseout (function(){  
+
+            blog.mouseout (function(){
             sousmenublog.attr('style','display:none');
-               
+
            });
-           
-            shop.mouseover (function(){ 
+
+            shop.mouseover (function(){
             sousmenushop.attr('style','display:block');
             mborder6.attr('style','display:block');
-               
+
            });
-           
-            shop.mouseout (function(){  
+
+            shop.mouseout (function(){
             sousmenushop.attr('style','display:none');
-               
+
            });
-           
-            components.mouseover (function(){ 
+
+            components.mouseover (function(){
             sousmenucomponents.attr('style','display:block');
             mborder7.attr('style','display:block');
-               
+
            });
-           
-            components.mouseout (function(){  
+
+            components.mouseout (function(){
             sousmenucomponents.attr('style','display:none');
-               
+
            });
- 
-           
+
+
        });
         </script>
-           
-           
-           
-    
+
+
+
+
     </body>
 </html>
