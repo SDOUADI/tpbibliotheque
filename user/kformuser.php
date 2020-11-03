ee5f1489b5343690b1e5a958b1b942cad0c5dc4b
SDOUADI <sdouadi@gmail.com>
2020-11-03 14:09:14 +0100

    update kformuser

--- user/kformuser.php
+++ user/kformuser.php
@@ -35,6 +35,7 @@ if(@$_GET['id']!=""){
 
         <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel="stylesheet" href="css/style.css">
+        
 
 
         <h1><?php echo $titreForm;?></h1>
@@ -49,15 +50,15 @@ if(@$_GET['id']!=""){
             </div>
             <div class="c100">
                 <label for="mail">Email : </label>
-                <input type="email" id="mail" name="email" value="<?php echo @$email;?>">
+                <input type="email" id="email" name="email" value="<?php echo @$email;?>"><span id='error_email' style="color:red"> </span><br/>
             </div>
             <div class="c100">
                 <label for="password">Mot de passe : </label>
-                <input type="password" id="password" name="password" value="<?php echo @$password;?>">
+                <input type="password" id="password" name="password" value="<?php echo @$password;?>"><span id='error_password' style="color:red"> </span>
             </div>
             <div class="c100">
                 <label for="password">Confirmer le mot de passe : </label>
-                <input type="password" id="confpassword" name="confpassword" "<?php echo @$confpassword;?>">
+                <input type="password" id="repassword" name="repassword" "<?php echo @$repassword;?>">
             </div>
             <div class="c100">
                 <label for="age">Age : </label>
@@ -97,6 +98,157 @@ if(@$_GET['id']!=""){
             </div>
         </form>
 </div>
-            
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>        
+<script>
+ 
+function validateEmail($email) {
+  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailReg.test( $email );
+}
+
+$(document).ready(function(){
+
+ $("#email").on("blur",function(){
+	 
+	 var $email= $("#email").val();
+	 
+		if(validateEmail($email)){
+			
+		$.ajax({
+
+     url : 'traitement.php',
+       type : 'GET',
+	   data:'email='+$("#email").val(),
+	   
+       dataType : 'text',
+       success : function(resultat, statut){
+			//alert(resultat);
+			if(resultat=="OK"){
+				//Mettre la bordure en vert
+				$("#email").css('color','green');
+				$('#error_email').html("");
+			}
+			else if(resultat=="KO"){
+				//Mettre la bordure en rouge
+					$("#email").css('color','red');
+					$('#error_email').html("Email already exist");
+			}
+       },
+
+       error : function(resultat, statut, erreur){
+			alert(resultat);
+       },
+
+       complete : function(resultat, statut){
+
+       }
+
+
+    });
+		}
+		else {
+			$('#email').focus();
+			$('#error_email').html("Email non Valide");
+		}
+	 
+ });
+ $("#password").on("input",function(){
+	 
+	 var $password= $("#password").val();
+	 
+	
+        		
+		$.ajax({
+
+     url : 'validatepassword.php',
+       type : 'GET',
+	   data:'password='+$("#password").val(),
+	   
+       dataType : 'text',
+       success : function(resultat, statut){
+			//alert(resultat);
+			if(resultat=="valid"){
+				//Mettre la bordure en vert
+				$("#password").css('color','green');
+				$('#error_password').html("");
+			}
+			else if(resultat!="valid"){
+				//Mettre la bordure en rouge
+					$("#password").css('color','red');
+					$('#error_password').html("Password not valid");
+			}
+       },
+
+       error : function(resultat, statut, erreur){
+			alert(resultat);
+       },
+
+       complete : function(resultat, statut){
+
+       }
+	   
+	   });
+	   
+	});
+
+    $("#repassword").on("input",function(){
+	 
+     var $password= $("#password").val();
+     var $repassword= $("#repassword").val();
+      
+             if($password==$repassword){
+                 //Mettre la bordure en vert
+                 $("#repassword").css('color','green');
+                 $('#error_repassword').html("");
+             }
+             else if($password!==$repassword){
+                 //Mettre la bordure en rouge
+                     $("#repassword").css('color','red');
+                     $('#error_repassword').html("Password not valid");
+             }
         
+        
+        });
+        
+     });      	
+
+	   
+
+
+
+
+
+/* $("#formulaire").submit(function(e){ // On sélectionne le formulaire par son identifiant
+    e.preventDefault(); 
+
+    var donnees = $(this).serialize(); // On créer une variable content le formulaire sérialisé
+     //alert(data);
+	// alert('ok');
+    $.ajax({
+
+     url : 'traitement.php',
+       type : 'POST',
+	   data:donnees,
+	   
+       dataType : 'html',
+       success : function(code_html, statut){
+			alert("Succees");
+       },
+
+       error : function(resultat, statut, erreur){
+			alert(resultat);
+       },
+
+       complete : function(resultat, statut){
+
+       }
+
+
+    }); 
+
+	
+});*/
+
+</script>      
       
